7543e83dfafaa74d992641e92d98a1820248c6db
andresmijares <andresmijares@gmail.com>
2018-01-03 14:38:37 -0300

    (FT) Added swagger docs for routes - Closes #1 #2 #3 #5

--- server/lib/routes/index.js
+++ server/lib/routes/index.js
@@ -1,126 +1,13 @@
-/*
-  GET /:subject DONE
-  POST /:subject/votes DONE
-  GET /:subject/votes DONE
-  GET /:subject/votes/:address
-*/
-
 const express = require('express')
 const router = express.Router()
-const validations = require('./validations')
 
-const handleError = (code, message, res) => {
-  return res
-    .status(code)
-    .json({message})
-}
+/* routes */
+const address = require('./address')
+const subject = require('./subject')
+const votes = require('./votes')
 
-router.get('/:subject', (req, res) => {
-  try {
-    const params = req.params
-    /**
-     * Business Logic, return votes
-     * 
-     */
-    res
-      .status(200)
-      .json({
-        ok: true,
-        statusCode: 200,
-        votes: null
-      })
+router.use(address)
+router.use(subject)
+router.use(votes)
 
-  } catch (error) {
-    res.status(500).json({ error: error.toString() })
-  }
-}) 
-
-
-router.post('/:subject/votes', (req, res) => {
-  try {
-    const stack = req.body
-    /* validate all parameters */
-    const validate = validations.validateSubject(stack)
-    if (!validate) handleError(400, 'Some parameters are missing', res)
-
-    /**
-     * Business Logic, return votes
-     * 
-     */
-    res
-      .status(200)
-      .json({
-        ok: true,
-        statusCode: 200,
-      })
-
-  } catch (error) {
-    res.status(500).json({ error: error.toString() })
-  }
-  })
-
-
-
-  router.get('/:subject', (req, res) => {
-  try {
-    const params = req.params
-    console.log(params)
-    /**
-     * Business Logic, return votes
-     * 
-     */
-    res
-      .status(200)
-      .json({
-        ok: true,
-        statusCode: 200
-      })
-
-  } catch (error) {
-    res.status(500).json({ error: error.toString() })
-  }
-})  
-
-router.get('/:subject/votes', (req, res) => {
-  try {
-    const params = req.params
-    /**
-     * Business Logic, return votes
-     * 
-     */
-    res
-      .status(200)
-      .json({
-        ok: true,
-        statusCode: 200,
-        votes: null
-      })
-
-  } catch (error) {
-    res.status(500).json({ error: error.toString() })
-  }
-})  
-
-router.get('/:subject/votes/:address', (req, res) => {
-  try {
-    const params = req.params
-    /**
-     * Business Logic, return votes
-     * 
-     */
-    res
-      .status(200)
-      .json({
-        ok: true,
-        statusCode: 200,
-        votes: null
-      })
-
-  } catch (error) {
-    res.status(500).json({ error: error.toString() })
-  }
-}) 
-
-
-
-  module.exports = router
+module.exports = router
