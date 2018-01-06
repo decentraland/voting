65f1a004d14e2f9cf3832bea7b09a3539a9708f8
Ignacio <ignaciomazzara@hotmail.com>
2018-01-06 00:29:53 -0300

    (FIX) update db schema

--- server/models/subject.js
+++ server/models/subject.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) =>
   sequelize.define('subject', {
     title: { type: DataTypes.STRING, allowNull: false },
-    description: { type: DataTypes.TEXT, allowNull: false },
-    image: { type: DataTypes.TEXT, allowNull: false },
+    description: { type: DataTypes.TEXT, allowNull: true },
+    image: { type: DataTypes.TEXT, allowNull: true },
   })
