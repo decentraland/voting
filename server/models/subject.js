aedfc8c6519c93cfeb2d76b4bc9e1a81bff3efcb
andresmijares <andresmijares@gmail.com>
2018-01-10 16:24:44 -0300

    (FT) Change subject id model by uuidv4, close #25

--- server/models/subject.js
+++ server/models/subject.js
@@ -1,6 +1,11 @@
 module.exports = (sequelize, DataTypes) =>
   sequelize.define('subject', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
     title: { type: DataTypes.STRING, allowNull: false },
     description: { type: DataTypes.TEXT, allowNull: true },
-    image: { type: DataTypes.TEXT, allowNull: true },
+    image: { type: DataTypes.TEXT, allowNull: true }
   })
